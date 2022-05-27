65c434fe89df457dd4b55c9d74cdc434b01ce85c
Trevor Cross <tjcross.work@gmail.com>
2022-05-27 15:45:38 -0500

    Update lift_data.py

--- lift_data.py
+++ lift_data.py
@@ -16,40 +16,84 @@ import numpy as np
 import pandas as pd
 
 # import toolbox functions
-from toolbox import make_request, build_url, build_filter, cart_prod
+from toolbox import *
 
 # ------------------------------
 # ---Define Data Dictionaries---
 # ------------------------------
 
+# connect to SF
+conn = connect_to_SF()
+
+# predefine API key
+api_key = "4vgTNfKfVVN0undVZijW9OZFNhmnucIYzSECsmrDW0RKGrdeA3+NiqbS7kAq3hUI"
+
 # define filters
-years = list(np.arange(2019,2022,1))
+years = list(np.arange(2000,2022,1))
 weeks = list(np.arange(1,21,1))
 
 # define base URL
 base_url = "https://api.collegefootballdata.com"
 
 # define list of sections
-sections = ['games']
+sections = ['records','calendar']
 
 # define dictionary of subsections
-subsection_dict = {'games':['media', 'players', 'teams']}
+subsection_dict = {'records':[''],
+                   'calendar':['']}
 
 # define filters for subsections
-filter_dict = {'media':['years','weeks'],
-               'players':None,
-               'teams':None}
+filter_dict = {'':['year']}
 
 # define filter plugins values dictionary
-plugin_dict = {'years':years,
-               'weeks':weeks}
+plugin_dict = {'year':years,
+               'week':weeks}
 
+# iterate through sections
 for sec in sections:
     
-    if subsection_dict[sec] is not None:
-        for subsec in subsection_dict[sec]:
+    # iterate through subsections
+    for subsec in subsection_dict[sec]:
             
-            if filter_dict[subsec] is not None:
-                plugins = list(map(lambda x:plugin_dict[x], filter_dict[subsec]))
-                result = cart_prod(plugins)
-                print(result)
+            # map subsections to their filters
+            filter_names = tuple(filter_dict[subsec])
+            filter_plugins = cart_prod(list(map(lambda x:plugin_dict[x], filter_dict[subsec])))
+            
+            # iterate through plugins
+            for plugin in filter_plugins:
+                
+                # build filter
+                final_filter = build_filter(filter_names, plugin)
+                
+                # build URL
+                url = build_url(base_url, sec, subsec, final_filter)
+                print(">>> URL: " + url)
+                
+                try:
+                    # send API request
+                    df = make_request(url, api_key)
+                    
+                except:
+                    
+                    # print status
+                    print(">>> Filter N/A")
+                
+                # append data to SF table if not empty
+                if len(df) > 0:
+                    
+                    # create table in SF
+                    if subsec != '':
+                        table_name = '_'.join([sec,subsec])
+                    else:
+                        table_name = sec
+                    
+                    try:
+                        create_table(conn, table_name, get_col_info(df))
+                    except:
+                        print()
+                        
+                    # append data to SF table
+                    append_data(conn, df, table_name)
+
+# close connection to SF        
+conn.close()
