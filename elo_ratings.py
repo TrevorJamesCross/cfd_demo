df3dce6abf97cbf61880c0c969649356c810e388
Trevor Cross <tjcross.work@gmail.com>
2022-06-15 09:37:50 -0500

    Update elo_ratings.py

--- elo_ratings.py
+++ elo_ratings.py
@@ -38,13 +38,16 @@ game_query = """select start_date, home_team, home_points, away_team, away_point
                 order by start_date"""
 game_df = pd.read_sql(game_query, conn)
 
+# close connection
+conn.close()
+
 # ------------------------
 # ---Run Elo Simulation---
 # ------------------------
 
 # define initial Elo rating
 init_rat = 1500
-reset_weight = 1 ## reset_weight=1 => no reset, reset_weight=0 => reset to init_rat
+reset_weight = 0.80 ## reset_weight=1 => no reset, reset_weight=0 => reset to init_rat
 
 # create dictionary to hold team Elo ratings
 team_rats = dict()
@@ -55,42 +58,53 @@ for game_num, game in tqdm(game_df.iterrows(), desc='Iterating games ', unit=' g
     # parse current date
     date = str(datetime.strptime(game['START_DATE'][0:10], '%Y-%m-%d').date())
 
-    # get current rating for home team
-    if game['HOME_TEAM'] in team_rats:
+    # if home team exists and in same season
+    if game['HOME_TEAM'] in team_rats and int(team_rats[game['HOME_TEAM']][-1][0][0:4]) == int(date[0:4]):
+        
+        # get current home rating
         home_rat = team_rats[game['HOME_TEAM']][-1][-1]
-
+    
+    # if home team exists and NOT in same season
+    elif game['HOME_TEAM'] in team_rats:
+        
+        # reset home rating
+        home_rat = reset_weight*(team_rats[game['HOME_TEAM']][-1][-1]-init_rat) + init_rat
+    
+    # if NOT home team exists
     else:
+        
+        # append home team to dict
         team_rats[game['HOME_TEAM']] = [(date, init_rat)]
         home_rat = team_rats[game['HOME_TEAM']][-1][-1]
     
-    # get current rating for away team
-    if game['AWAY_TEAM'] in team_rats:
-        away_rat = team_rats[game['AWAY_TEAM']][-1][-1]
-
-    else:
-        team_rats[game['AWAY_TEAM']] = [(date, init_rat)]
+    # if away team exists and in same season
+    if game['AWAY_TEAM'] in team_rats and int(team_rats[game['AWAY_TEAM']][-1][0][0:4]) == int(date[0:4]):
+        
+        # get current home rating
         away_rat = team_rats[game['AWAY_TEAM']][-1][-1]
     
-    # season change => percent reduction to initial rate
-    try:
-        if date[0:4] == str(int(past_year)+1):
-            home_rat = reset_weight*(home_rat-init_rat) + init_rat
-            away_rat = reset_weight*(away_rat-init_rat) + init_rat
-    except:
-        pass
+    # if away team exists and NOT in same season
+    elif game['AWAY_TEAM'] in team_rats:
+        
+        # reset away rating
+        away_rat = reset_weight*(team_rats[game['AWAY_TEAM']][-1][-1]-init_rat) + init_rat
+    
+    # if NOT away team exists
+    else:
+        
+        # append away team to dict
+        team_rats[game['AWAY_TEAM']] = [(date, init_rat)]
+        away_rat = team_rats[game['AWAY_TEAM']][-1][-1]
     
     # calculate score margin from game
     margin = game['HOME_POINTS'] - game['AWAY_POINTS']
         
     # get new ratings
     home_rat_new, away_rat_new = calc_new_rats(home_rat, away_rat, margin)
-        
+    
     # append new ratings to dict
     team_rats[game['HOME_TEAM']].append((date, home_rat_new))
     team_rats[game['AWAY_TEAM']].append((date, away_rat_new))
-    
-    # save current year as past year
-    past_year = date[0:4]
     
 # ------------------
 # ---Plot Results---
