8dfe415c55808b70a7b65ecb0aeb8bf0b0f4b406
Trevor Cross <tjcross.work@gmail.com>
2022-06-17 15:44:34 -0500

    Update elo_ratings.py

--- elo_ratings.py
+++ elo_ratings.py
@@ -1,7 +1,7 @@
 """
 College Football Data Analytics: Elo Ratings
 Author: Trevor Cross
-Last Updated: 06/16/22
+Last Updated: 06/17/22
 
 Simuates NCAAF games using an Elo rating algorithm.
 """
@@ -18,14 +18,10 @@ import pandas as pd
 from tqdm import tqdm
 from datetime import datetime
 from operator import itemgetter
-from sklearn.metrics import log_loss
+from sklearn.metrics import log_loss, accuracy_score
 
 # import toolbox functions
 from toolbox import *
-
-# remove warnings
-import warnings
-warnings.filterwarnings("ignore")
 
 # ---------------------------
 # ---Pull Game Information---
@@ -35,10 +31,20 @@ warnings.filterwarnings("ignore")
 conn = connect_to_SF()
 
 # obtain game data
-game_query = """select start_date, home_team, home_points, away_team, away_points from games
-                where season >= 1971
-                order by start_date"""
+game_query = """
+             select start_date, home_team, home_points, away_team, away_points from games
+             where season >= 1920 and season <= 2019
+             order by start_date
+             """
+                
 game_df = pd.read_sql(game_query, conn)
+
+# obtain fbs team list
+fbs_query = """
+            select school from teams_fbs
+            """
+
+fbs_team_list = pd.read_sql(fbs_query, conn)['SCHOOL'].tolist()
 
 # close connection
 conn.close()
@@ -47,9 +53,8 @@ conn.close()
 # ---Run Elo Simulation---
 # ------------------------
 
-# define initial Elo rating
-init_rat = 1500
-reset_weight = 0.75 ## reset_weight=1 => no reset, reset_weight=0 => reset to init_rat
+# define reset weight
+retain_weight = 1.00 ## retain_weight=1 => no reset, retain_weight=0 => reset to init_rat
 
 # create dictionary to hold team Elo ratings
 team_rats = dict()
@@ -70,10 +75,13 @@ for game_num, game in tqdm(game_df.iterrows(), desc='Iterating games ', unit=' g
     elif game['HOME_TEAM'] in team_rats:
         
         # reset home rating
-        home_rat = reset_weight*(team_rats[game['HOME_TEAM']][-1][1]-init_rat) + init_rat
+        home_rat = retain_weight*(team_rats[game['HOME_TEAM']][-1][1]-init_rat) + init_rat
     
     # if NOT home team exists
     else:
+        
+        # get initial rating
+        init_rat = get_init_rat(game['HOME_TEAM'], fbs_team_list)
         
         # append home team to dict
         team_rats[game['HOME_TEAM']] = [(date, init_rat, None, None)]
@@ -89,10 +97,13 @@ for game_num, game in tqdm(game_df.iterrows(), desc='Iterating games ', unit=' g
     elif game['AWAY_TEAM'] in team_rats:
         
         # reset away rating
-        away_rat = reset_weight*(team_rats[game['AWAY_TEAM']][-1][1]-init_rat) + init_rat
+        away_rat = retain_weight*(team_rats[game['AWAY_TEAM']][-1][1]-init_rat) + init_rat
     
     # if NOT away team exists
     else:
+        
+        # get initial rating
+        init_rat = get_init_rat(game['AWAY_TEAM'], fbs_team_list)
         
         # append away team to dict
         team_rats[game['AWAY_TEAM']] = [(date, init_rat, None, None)]
@@ -102,7 +113,7 @@ for game_num, game in tqdm(game_df.iterrows(), desc='Iterating games ', unit=' g
     margin = game['HOME_POINTS'] - game['AWAY_POINTS']
 
     # calc new ratings
-    home_info, away_info = calc_new_rats(home_rat, away_rat, margin)
+    home_info, away_info = calc_new_rats(home_rat, away_rat, margin, K=50)
     home_rat_new, home_conf, home_act = home_info
     away_rat_new, away_conf, away_act = away_info
     
@@ -127,29 +138,28 @@ acts = [act for act in acts if act != None]
 
 # remove ties
 while len(np.unique(acts)) > 2:
+    # for some (dumb) reason, the below loop will not remove all 0.5's & related
+    # indices the first go around. So it's ran until we get only the binary
     for index, value in enumerate(acts):
         if value == 0.5:
             del preds[index]
             del acts[index]
 
 # split train and test data
-train_preds, test_preds = preds[:int(0.8*len(preds))], preds[int(0.8*len(preds)):]
-train_acts, test_acts = acts[:int(0.8*len(acts))], acts[int(0.8*len(acts)):]
+train_preds, test_preds = preds[:int(0.7*len(preds))], preds[int(0.7*len(preds)):]
+train_acts, test_acts = acts[:int(0.7*len(acts))], acts[int(0.7*len(acts)):]
 
-# calc log loss
-train_loss = log_loss(acts, preds)
-print("\n >>> Log Loss (Train Data): {}".format(train_loss))
+# calc log loss & accuracy
+if True:
+    train_loss = log_loss(train_acts, train_preds)
+    print("\n >>> Log Loss (Train Data): {}".format(train_loss))
+    
+    train_acc = accuracy_score(train_acts, list(map(round,train_preds)))
+    print("\n >>> Accuracy (Train Data): {}".format(train_acc))
 
 if False:
     test_loss = log_loss(test_acts, test_preds)
     print("\n >>> Log Loss (Test Data): {}".format(test_loss))
     
-# ------------------
-# ---Plot Results---
-# ------------------
-
-# # define team to plot
-# team_name = 'Wisconsin'
-
-# # plot ratings against date
-# plot_rats(team_rats, team_name)
+    test_acc = accuracy_score(test_acts, list(map(round,test_preds)))
+    print("\n >>> Accuracy (Test Data): {}".format(test_acc))
