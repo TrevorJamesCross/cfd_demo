7eaa7209e17b82fb13d244ceb41fb1c656faafe3
Trevor Cross <tjcross.work@gmail.com>
2022-06-22 17:59:47 -0500

    Update DWCC_snowflake.sh
    Obfuscated info

--- platform_integrations/data_dot_world/DWCC_snowflake.sh
+++ platform_integrations/data_dot_world/DWCC_snowflake.sh
@@ -3,10 +3,10 @@ sudo docker run -it --rm \
 --mount type=bind,source=${PWD}/sf_tmp,target=/app/log \
 datadotworld/dwcc:latest catalog-snowflake \
 --account=aebs-dev \
---server=aebs.us-east-1.snowflakecomputing.com \
---user=trevor.cross \
---password=Trevor!=720 \
---database=CFB_DEMO \
+--server=<server> \
+--user=<user> \
+--password=<password> \
+--database=<database> \
 --all-schemas \
 --role=SYSADMIN \
 --name=sf_catalog \
