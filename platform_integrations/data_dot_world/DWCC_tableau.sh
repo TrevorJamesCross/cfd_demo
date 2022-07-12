154141610ca182882cc83aed1ff4528fe1fab019
tjcross <tjcross.work@gmail.com>
2022-07-12 16:51:49 -0500

    Fixed it

--- platform_integrations/data_dot_world/DWCC_tableau.sh
+++ platform_integrations/data_dot_world/DWCC_tableau.sh
@@ -2,11 +2,11 @@ sudo docker run -it --rm \
 --mount type=bind,source=${PWD}/tmp_dir,target=/dwcc-output \
 --mount type=bind,source=${PWD}/tmp_dir,target=/app/log \
 datadotworld/dwcc-cert:latest catalog-tableau \
---account=aebs-dev \
---tableau-api-base-url=https://tableau.aebsinternal.com/api/3.15 \
+--account= \
+--tableau-api-base-url= \
 --tableau-site=AEBS \
---tableau-username=trevor.cross \
---tableau-password=iplayedwithmydogtoday \
+--tableau-username= \
+--tableau-password= \
 --tableau-project="CFB Dashboards" \
 --name=tableau_catalog \
 --output=/dwcc-output
