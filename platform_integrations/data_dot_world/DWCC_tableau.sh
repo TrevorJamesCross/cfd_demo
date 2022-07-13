3fb27c2c1cc700780f41878148d8b4c3c6863228
tjcross <tjcross.work@gmail.com>
2022-07-13 10:43:35 -0500

    Solved catalogging issue (despite their best efforts), updated elo rating code

--- platform_integrations/data_dot_world/DWCC_tableau.sh
+++ platform_integrations/data_dot_world/DWCC_tableau.sh
@@ -2,11 +2,11 @@ sudo docker run -it --rm \
 --mount type=bind,source=${PWD}/tmp_dir,target=/dwcc-output \
 --mount type=bind,source=${PWD}/tmp_dir,target=/app/log \
 datadotworld/dwcc-cert:latest catalog-tableau \
---account= \
---tableau-api-base-url= \
---tableau-site=AEBS \
---tableau-username= \
---tableau-password= \
---tableau-project="CFB Dashboards" \
+--account=aebs-dev \
+--tableau-api-base-url=<url> \
+--tableau-site=<site> \
+--tableau-username=<user> \
+--tableau-password=<password> \
+--tableau-project=<project> \
 --name=tableau_catalog \
 --output=/dwcc-output
