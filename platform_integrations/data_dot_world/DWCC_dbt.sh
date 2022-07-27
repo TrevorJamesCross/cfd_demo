938ccfd60c5ba3863f762a2e643187271f493baa
Trevor Cross <tjcross.work@gmail.com>
2022-07-27 16:52:55 -0500

    Update DWCC_dbt.sh

--- platform_integrations/data_dot_world/DWCC_dbt.sh
+++ platform_integrations/data_dot_world/DWCC_dbt.sh
@@ -3,9 +3,9 @@ sudo docker run -it --rm \
 --mount type=bind,source=${PWD}/tmp_dir,target=/dwcc-output \
 datadotworld/dwcc:latest catalog-dbt \
 --account=aebs-dev \
---dbt-profile-file=/dwcc-input/profiles.yml \
+--profile-file=/dwcc-input/profiles.yml \
 --profile=aebs_trevor \
 --target=dev \
---data-dir=/dwcc-input \
+--artifact-directory=/dwcc-input \
 --name=dbt_catalog \
 --output=/dwcc-output
